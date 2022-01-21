c331e1bfcafea4fea710da7a3ea48ce359baa3a8
Roman Musin <roman9m@yandex.ru>
2022-01-21 14:20:04 +0300

    routes

--- backend/app.js
+++ backend/app.js
@@ -44,10 +44,9 @@ app.use((req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
-  res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', origin);
   }
   const { method } = req;
-
 
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
@@ -60,8 +59,8 @@ app.use((req, res, next) => {
 
   next();
 });
-*/
-app.use(cors());
+
+app.use(cors(allowedCors));
 
 app.use(cookieParser());
 app.use(express.json());
