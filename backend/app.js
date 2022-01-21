f50296ce3dd772d53df43a62cb9b098ee314435c
Roman Musin <roman9m@yandex.ru>
2022-01-21 09:03:19 +0300

    routes

--- backend/app.js
+++ backend/app.js
@@ -18,9 +18,10 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+/*
 const options = {
   origin: [
-    'http://localhost:5000',
+    'http://localhost:3000',
     'http://romus.mesto.nomoredomains.work',
     'https://romus.mesto.nomoredomains.work',
   ],
@@ -30,8 +31,37 @@ const options = {
   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
   credentials: true,
 };
+*/
 
-app.use('*', cors(options));
+const allowedCors = [
+  'http://romus.mesto.nomoredomains.work',
+  'https://romus.mesto.nomoredomains.work',
+  'localhost:3000',
+];
+
+// eslint-disable-next-line consistent-return
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+  const { method } = req;
+
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
+});
+
+app.use('*', cors(allowedCors));
 
 app.use(cookieParser());
 app.use(express.json());
