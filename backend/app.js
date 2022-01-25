ec71075f1d6f299b0b5893650db779b225cfbe91
Roman Musin <roman9m@yandex.ru>
2022-01-25 09:00:43 +0300

    front fix

--- backend/app.js
+++ backend/app.js
@@ -56,7 +56,7 @@ app.use(cors({
   preflightContinue: false,
   optionsSuccessStatus: 204,
   allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Cookie'],
-  exposedHeaders: ['Set-Cookie', 'Authorization'],
+  exposedHeaders: ['Set-Cookie'],
   credentials: true,
 }));
 
