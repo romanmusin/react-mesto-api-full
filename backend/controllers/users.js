1ba447f292d6b1c0bc631b662a861744f70f4b73
Roman Musin <roman9m@yandex.ru>
2022-01-25 15:30:38 +0300

    got cookie

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -172,7 +172,7 @@ module.exports.login = (req, res, next) => {
               maxAge: 3600000 * 24 * 7,
               secure: true,
               sameSite: 'none',
-              domain: 'romus.mesto.nomoredomains.work',
+              // domain: 'romus.mesto.nomoredomains.work',
             })
             .send({ message: 'Вход совершен успешно' });
         })
