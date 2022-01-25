e3eb5d45cb9f43090215872e3e53a3819bbf84f3
Roman Musin <roman9m@yandex.ru>
2022-01-26 00:14:22 +0300

    got cookie

--- backend/app.js
+++ backend/app.js
@@ -18,34 +18,6 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-/*
-const allowedCors = [
-  'http://romus.mesto.nomoredomains.work',
-  'https://romus.mesto.nomoredomains.work',
-  'localhost:3000',
-];
-
-// eslint-disable-next-line consistent-return
-app.use((req, res, next) => {
-  // const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  // if (allowedCors.includes(origin)) {
-  // }
-  res.header('Access-Control-Allow-Origin', '*');
-  const { method } = req;
-
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-
-  next();
-});
-*/
 app.use('*', cors({
   origin: [
     'http://romus.mesto.nomoredomains.work',
