cca8a04951be6147411a46cce41db4738e9a94a2
Roman Musin <roman9m@yandex.ru>
2022-01-29 01:03:23 +0300

    env fix

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -186,7 +186,7 @@ module.exports.login = (req, res, next) => {
 
           const token = jwt.sign(
             { _id: user._id },
-            NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret-key',
+            NODE_ENV === 'production' ? JWT_SECRET : 'secret-key',
             { expiresIn: '7d' },
           );
           return res
